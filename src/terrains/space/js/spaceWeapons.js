3fc4ca3b435a8a1db9e0b9bbc177703fa5866557
Ben Max <beltomax@mailfence.com>
2026-04-30 16:56:31 +1000

    Update spaceWeapons.js

--- src/terrains/space/js/spaceWeapons.js
+++ src/terrains/space/js/spaceWeapons.js
@@ -655,7 +655,42 @@ export class GuassRound extends Fish {
       [0, 3, 0],
       [2, 3, 0]
     ]
+    this.splashCoords = this.addNeighbours(
+      null,
+      3,
+      3,
+      [],
+      [
+        [3, 3, 2],
+        [0, 0, 20],
+        [1, 1, 20],
+        [2, 2, 0],
+        [4, 4, 1],
+        [5, 5, 0]
+      ]
+    )
 
+    this.crashCoords = this.addNeighbours(
+      null,
+      3,
+      3,
+      [],
+      [
+        [0, 0, 20],
+        [1, 1, 20],
+        [2, 2, 20],
+        [2, 3, 0],
+        [3, 1, 0],
+        [3, 2, 1],
+        [3, 3, 2],
+        [3, 4, 1],
+        [3, 5, 0],
+        [4, 2, 0],
+        [4, 3, 1],
+        [4, 4, 0],
+        [5, 3, 0]
+      ]
+    )
     // Tracks crash location when round hits land
     this.crashLoc = null
   }
