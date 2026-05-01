32e38a60b39deec43af906586a63a2b97da00844
Ben Max <beltomax@mailfence.com>
2026-05-02 00:24:49 +1000

    Update print.js

--- src/docs/print.js
+++ src/docs/print.js
@@ -1,25 +1,13 @@
 import { fetchNavBar } from '../navbar/navbar.js'
 import { setupPrint } from './setupPrint.js'
-import { showShipInfo } from './shipprint.js'
-import { showWeapons } from './weaponprint.js'
-import { enemy } from '../waters/enemy.js'
-
-/**
- * @typedef {Object} FleetEntity
- * @property {Array} ships - Array of ship objects
- * @property {Object} loadOut - Loadout configuration
- * @property {Object} UI - UI interface
- */
 
 /**
  * Initializes the print page display
  */
-export async function initializePrintPage () {
+async function initializePrintPage () {
   await loadNavigation()
-  const { printMap, friendFleet } = await setupPrint()
-  if (printMap) {
-    displayFleetInformation(friendFleet)
-  }
+  await setupPrint()
+
   globalThis.print()
 }
 
@@ -29,16 +17,6 @@ export async function initializePrintPage () {
  */
 async function loadNavigation () {
   await fetchNavBar('print', 'Battleship')
-}
-
-/**
- * Displays ship and weapon information for both fleets
- */
-function displayFleetInformation (friend) {
-  // showShipInfo(friend)
-  // showShipInfo(enemy)
-  // showWeapons(friend)
-  // showWeapons(enemy)
 }
 
 // Initialize on module load
