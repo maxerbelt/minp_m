3e5f71678c90294ae35b8f89ee683b4c7d6c59c9
Ben Max <beltomax@mailfence.com>
2026-06-04 05:32:33 +1000

    Update TriMultiBitMorphology.js

--- src/grid/morphology/strategies/TriMultiBitMorphology.js
+++ src/grid/morphology/strategies/TriMultiBitMorphology.js
@@ -99,7 +99,7 @@ export class TriMultiBitMorphology {
    * const morph = new TriMultiBitMorphology(mask, store, bits, width, height, indexer);
    */
   constructor (mask, store, bits, width, height, indexer) {
-    if (!store || !store.isMultiBit) {
+    if (!store?.isMultiBit) {
       throw new Error('TriMultiBitMorphology: store must be multi-bit')
     }
     this.mask = mask
